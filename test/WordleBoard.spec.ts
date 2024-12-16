dc6c68f9862e72986688213997e23e72046b9c91
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-16 14:56:29 -0300

    extract variable

--- test/WordleBoard.spec.ts
+++ test/WordleBoard.spec.ts
@@ -5,9 +5,10 @@ import WordleBoard from '../app/components/WordleBoard.vue'
 import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from './settings'
 
 describe('WordleBoard', () => {
+  let wordOfTheDay: string = 'TESTS'
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
     const wrapper = mount(WordleBoard, { props: {
-      wordOfTheDay: "TESTS"
+      wordOfTheDay
     }})
 
     const guessInput = wrapper.find("input[type=text]")
@@ -19,7 +20,7 @@ describe('WordleBoard', () => {
 
   test('a defeat message appears when the user makes a guess that does not match the word of the day', async () => {
     const wrapper = mount(WordleBoard, { props: {
-      wordOfTheDay: "TESTS"
+      wordOfTheDay
     }})
 
     const guessInput = wrapper.find("input[type=text]")
