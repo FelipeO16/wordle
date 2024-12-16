a970dac4fc3a22a69055fc8aaa16e331992cf82c
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-16 14:31:24 -0300

    chore: remove clutter

--- app/components/WordleBoard.spec.ts
+++ app/components/WordleBoard.spec.ts
@@ -5,17 +5,14 @@ import WordleBoard from './WordleBoard.vue'
 
 describe('WordleBoard', () => {
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    // arrange
     const wrapper = mount(WordleBoard, { props: {
       wordOfTheDay: "TESTS"
     }})
 
-    // act
     const guessInput = wrapper.find("input[type=text]")
     await guessInput.setValue("TESTS")
     await guessInput.trigger("keydown.enter")
 
-    // assert
     expect(wrapper.text()).toContain("You won!")
   })
 })
