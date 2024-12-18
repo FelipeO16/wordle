b5f809a7a819d61597fa24ac52bf559f8ad32948
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-18 10:23:07 -0300

    Extract constant

--- shared/settings/index.ts
+++ shared/settings/index.ts
@@ -1,4 +1,4 @@
 export const VICTORY_MESSAGE = "🎉 Congratulations! 🎉"
 export const DEFEAT_MESSAGE = 'Nope! Try again.';
-export const MAX_GUESS_ATTEMPTS = 8;
+export const MAX_GUESS_ATTEMPTS = 6;
 export const WORD_LENGTH = 5;
