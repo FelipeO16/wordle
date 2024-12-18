4b081e1050fd415d1481de305408cca9ea745987
Felipe Oliveira <felipe16.12.1998@gmail.com>
2024-12-18 14:48:03 -0300

    Add card flipping animation when a guess is submitted

--- app/components/GuessView.vue
+++ app/components/GuessView.vue
@@ -3,7 +3,7 @@ import {WORD_LENGTH} from "#shared/settings"
 import words from "#shared/settings/words.json"
 import {computed, ref, triggerRef} from "vue"
 
-defineProps<{guess: string}>()
+withDefaults(defineProps<{ guess: string, shouldFlip?: boolean }>(), {shouldFlip: false})
 </script>
 
 <template>
@@ -11,12 +11,13 @@ defineProps<{guess: string}>()
     <li v-for="(letter, index) in guess.padEnd(WORD_LENGTH, ' ')"
         :key="`${letter}-${index}`"
         :data-letter="letter"
+        :class="{'with-flips': shouldFlip}"
         class="letter"
         v-text="letter"/>
   </ul>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .word {
   list-style: none;
   padding: 0;
@@ -29,6 +30,9 @@ defineProps<{guess: string}>()
   width: 5rem;
   height: 5rem;
   display: flex;
+  --front-color: hsl(0, 0%, 99%);
+  --back-color: hsl(0, 0%, 70%);
+  background-color: var(--front-color);
   justify-content: center;
   align-items: center;
   font-size: 2rem;
@@ -43,6 +47,31 @@ li:not([data-letter=" "]) {
   }
   50% {
     transform: scale(1.4);
+  }
+}
+
+$maxWordSize: 5;
+@for $i from 1 through $maxWordSize {
+  .with-flips:nth-of-type(#{$i}) {
+    animation: flip-card 300ms forwards;
+    animation-delay: #{250*$i}ms;
+  }
+}
+@keyframes flip-card {
+  0% {
+    transform: rotateY(0);
+    background-color: var(--front-color);
+  }
+  49% {
+    background-color: var(--front-color);
+  }
+  50% {
+    transform: rotateY(-90deg);
+    background-color: var(--back-color);
+  }
+  100% {
+    transform: rotateY(0);
+    background-color: var(--back-color);
   }
 }
 </style>
